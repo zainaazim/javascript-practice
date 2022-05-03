cd75175426830d29db209a35045a97da0faae59d
Chase Carnaroli <chasecarnaroli@gmail.com>
2022-05-03 07:18:28 -0700

    Add comments

--- test.js
+++ test.js
@@ -1,21 +1,44 @@
-// Total number of problems in ./problems
-const numberOfProblems = 12
+/**
+ * TEST DRIVER
+ * 
+ * This file runs the test cases for the problems/ folder.
+ * You should NOT need to edit this file.
+ *  
+ */
 
-// Run a test
+// Total number of problems in ./problems
+const numberOfProblems = 16
+
+/**
+ * Run Test
+ * 
+ * Given a file number, run the test cases for that problem.
+ * Outputs "Problem ${number}: Passed ${number of tests passed}/${number of tests}"
+ * For each failed test,
+ *  Outputs "Input ${test} returned ${result} but expected ${expectedResult}"
+ * 
+ * @param {*} number of the file
+ */
 function runTest(number) {
+    // Get the problem and test cases from the file
     const { problem, tests } = require(`./problems/${number}`);
+    
+    // Initialize passed counter and failed list
     let passed = 0;
     let failed = [];
 
     // Run each test case
     for (const test of tests) {
         const [input, expected] = test;
+
         const expectedResult = JSON.stringify(expected)
         const result = JSON.stringify(problem(input));
 
         if (result === expectedResult) {
+            // Result Passed
             passed += 1;
         } else {
+            // Result Failed
             failed.push(`Input ${JSON.stringify(input)} returned ${result} but expected ${expectedResult}`)
         }
     }
@@ -30,7 +53,7 @@ function runTest(number) {
     }
 }
 
-// Get arguments
+// Get arguments from the command line
 const args = process.argv.slice(2).map(arg => parseInt(arg));
 
 if (args.length) {
