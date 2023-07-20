f0334a6d3d06a6b9b14f39c03dc1f1ceeaa01b5f
zaina azim <zainaazim9@gmail.com>
2023-07-20 14:43:24 -0700

    completed problem 9

--- problems/9.js
+++ problems/9.js
@@ -7,7 +7,16 @@
  * @example [{name: "Chase", grade: 89}, {name: "Raman", grade: 92}, {name: "Peter", grade: 100}] -> ["Peter", "Raman"]
 */
 function problem(students) {
-    return null;
+
+    const filtered = students.filter(function(item) {
+        return item.grade > 90;
+    });
+
+    const names = filtered.map(function(item) {
+        return item.name;
+    });
+
+    return names.sort();
 }
 
 const tests = [
